0accf433e9298987395e9e664ef268299aae5b3f
Maria Şuteu <mara.suteu@gmail.com>
2020-05-07 21:41:17 +0300

    client

--- L5/Client.h
+++ L5/Client.h
@@ -1,13 +1,26 @@
 #pragma once
+#include<vector>
 #include "Film.h"
+#include "Admin.h"
+
+using namespace std;
 
 class Client
 {
+	friend class Admin;
 private:
+	Admin liste;
+	struct Item {
+		Film film;
+		bool seen;
+	};
+	vector<Item> watchlist;
+public:
+
 	Client();
 	~Client();
-
-	Film* liste;
-
+	
+	void seen(string genre);
+	//ShellExecute(NULL, "open", "http://dreamincode.net",NULL, NULL, SW_SHOWNORMAL);-pt pornit trailer in browser
 };
 
